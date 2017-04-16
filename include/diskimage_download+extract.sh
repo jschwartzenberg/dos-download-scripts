98edb00b986d2988629a0f83961a533be9c8af74
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2017-04-16 22:28:45 +0200

    msoffice 4.0 trick

--- include/diskimage_download+extract.sh
+++ include/diskimage_download+extract.sh
@@ -6,6 +6,7 @@ download_and_extract()
   IMGURL=$1
   IMGCNT=$2
   DESTINATION=$3
+  SEPARATE_DIRS=$4
 
   mkdir $TMP_DIR
   for i in $(seq 1 "$IMGCNT")
@@ -14,6 +15,11 @@ download_and_extract()
     mkdir $TMP_DIR/ex$i
     extract_7z $TMP_DIR/disk$i.zip "" $TMP_DIR/ex$i
     IMAGE="${TMP_DIR}/ex$i/`ls \"${TMP_DIR}\"/ex$i/`"
-    mcopy -sn -i "$IMAGE" ::* $3
+    if [ $SEPARATE_DIRS = true ]; then
+      mkdir $DESTINATION/disk$i
+      mcopy -sn -i "$IMAGE" ::* $DESTINATION/disk$i
+    else
+      mcopy -sn -i "$IMAGE" ::* $DESTINATION
+    fi
   done
 }
