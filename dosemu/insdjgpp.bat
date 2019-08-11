946c04f954f26d1df447996a20c9bcc4a8b676c4
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2019-08-11 09:51:58 +0200

    Remove hardcoded C drive, use variable for URL which is supported by the newer comcom32.

--- dosemu/insdjgpp.bat
+++ dosemu/insdjgpp.bat
@@ -1,27 +1,33 @@
 @echo off
 
-mkdir c:\inst
-mkdir c:\inst\djgpp
+set DEFAULT_MIRROR=https://na.mirror.garr.it/mirrors/djgpp/current
 
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/unzip32.exe -O ${DOSEMU2_DRIVE_C}/inst/djgpp/unzip32.exe
+set INST_DIR=%USERDRV%:\inst\djgpp
+set D2DU=${DOSEMU2_DRIVE_%USERDRV%}
+set INST_DIR_UNIX=%D2DU%/inst/djgpp
 
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2/djdev205.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/djdev205.zip
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2apps/rhid15ab.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/rhid15ab.zip
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2gnu/bnu232b.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/bnu232b.zip
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2gnu/gcc910b.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/gcc910b.zip
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2gnu/gdb801b.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/gdb801b.zip
-unix eval wget http://na.mirror.garr.it/mirrors/djgpp/current/v2gnu/mak421b.zip -O ${DOSEMU2_DRIVE_C}/inst/djgpp/mak421b.zip
+mkdir %USERDRV%:\inst
+mkdir %INST_DIR%
+
+unix eval wget %DEFAULT_MIRROR%/unzip32.exe -O %INST_DIR_UNIX%/unzip32.exe
+
+unix eval wget %DEFAULT_MIRROR%/v2/djdev205.zip -O %INST_DIR_UNIX%/djdev205.zip
+unix eval wget %DEFAULT_MIRROR%/v2apps/rhid15ab.zip -O %INST_DIR_UNIX%/rhid15ab.zip
+unix eval wget %DEFAULT_MIRROR%/v2gnu/bnu232b.zip -O %INST_DIR_UNIX%/bnu232b.zip
+unix eval wget %DEFAULT_MIRROR%/v2gnu/gcc910b.zip -O %INST_DIR_UNIX%/gcc910b.zip
+unix eval wget %DEFAULT_MIRROR%/v2gnu/gdb801b.zip -O %INST_DIR_UNIX%/gdb801b.zip
+unix eval wget %DEFAULT_MIRROR%/v2gnu/mak421b.zip -O %INST_DIR_UNIX%/mak421b.zip
 
 mkdir djgpp
 cd djgpp
 
-c:\inst\djgpp\unzip32 c:\inst\djgpp\djdev205.zip
-c:\inst\djgpp\unzip32 c:\inst\djgpp\rhid15ab.zip
-c:\inst\djgpp\unzip32 c:\inst\djgpp\bnu232b.zip
-c:\inst\djgpp\unzip32 -o c:\inst\djgpp\gcc910b.zip
-c:\inst\djgpp\unzip32 c:\inst\djgpp\gdb801b.zip
-c:\inst\djgpp\unzip32 c:\inst\djgpp\mak421b.zip
+%INST_DIR%\unzip32 %INST_DIR%\djdev205.zip
+%INST_DIR%\unzip32 %INST_DIR%\rhid15ab.zip
+%INST_DIR%\unzip32 %INST_DIR%\bnu232b.zip
+%INST_DIR%\unzip32 -o %INST_DIR%\gcc910b.zip
+%INST_DIR%\unzip32 %INST_DIR%\gdb801b.zip
+%INST_DIR%\unzip32 %INST_DIR%\mak421b.zip
 
-echo path c:\djgpp\bin;%%PATH%% > c:\envdjgpp.bat
-echo set DJGPP=c:\djgpp\djgpp.env >> c:\envdjgpp.bat
-echo call envdjgpp.bat >> c:\userhook.bat
+echo path %USERDRV%:\djgpp\bin;%%PATH%% > %USERDRV%:\envdjgpp.bat
+echo set DJGPP=%USERDRV%:\djgpp\djgpp.env >> %USERDRV%:\envdjgpp.bat
+echo call envdjgpp.bat >> %USERDRV%:\userhook.bat
